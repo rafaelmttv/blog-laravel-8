892e046862563436876aac27669dd0ad4f448eb0
Rafaelmttv <rafaelmttv@gmail.com>
2022-09-19 15:07:07 -0300

    Just installed Breeze package

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,7 +25,7 @@
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
-                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
+                        <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
                     @else
                         <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
 
@@ -46,9 +46,9 @@
                 </div>
 
                 <br>
-                <div>
+                {{-- <div>
                     <a href="{{ route('company') }}">{{$company}}</a>
-                </div>
+                </div> --}}
                 <br>
                 {{ env('APP_NAME') }}
 
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,9 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
-Route::get('/company', [HomeController::class, 'company'])->name('company');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
